862b57fbb20e77bfbdf239a8dc584c475cd86fbd
Mikolaj Dobrucki <mikolaj.dobrucki@gmail.com>
2020-01-19 21:27:15 +0100

    Add Google Maps

--- src/pages/about/index.md
+++ src/pages/about/index.md
@@ -2,6 +2,9 @@
 templateKey: 'about-page'
 path: /about
 title: About our values
+mapZoom: 5
+mapLat: 19.95
+mapLng: 10.33
 ---
 ### Shade-grown coffee
 Coffee is a small tree or shrub that grows in the forest understory in its wild form, and traditionally was grown commercially under other trees that provided shade. The forest-like structure of shade coffee farms provides habitat for a great number of migratory and resident species.
--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -3,8 +3,122 @@ import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
+import GoogleMap from "google-map-react";
 
-export const AboutPageTemplate = ({ title, content, contentComponent }) => {
+const mapOptions = {
+  styles: [
+    { elementType: "geometry", stylers: [{ color: "#ebe3cd" }] },
+    { elementType: "labels.text.fill", stylers: [{ color: "#523735" }] },
+    { elementType: "labels.text.stroke", stylers: [{ color: "#f5f1e6" }] },
+    {
+      featureType: "administrative",
+      elementType: "geometry.stroke",
+      stylers: [{ color: "#c9b2a6" }]
+    },
+    {
+      featureType: "administrative.land_parcel",
+      elementType: "geometry.stroke",
+      stylers: [{ color: "#dcd2be" }]
+    },
+    {
+      featureType: "administrative.land_parcel",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#ae9e90" }]
+    },
+    {
+      featureType: "landscape.natural",
+      elementType: "geometry",
+      stylers: [{ color: "#dfd2ae" }]
+    },
+    {
+      featureType: "poi",
+      elementType: "geometry",
+      stylers: [{ color: "#dfd2ae" }]
+    },
+    {
+      featureType: "poi",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#93817c" }]
+    },
+    {
+      featureType: "poi.park",
+      elementType: "geometry.fill",
+      stylers: [{ color: "#a5b076" }]
+    },
+    {
+      featureType: "poi.park",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#447530" }]
+    },
+    {
+      featureType: "road",
+      elementType: "geometry",
+      stylers: [{ color: "#f5f1e6" }]
+    },
+    {
+      featureType: "road.arterial",
+      elementType: "geometry",
+      stylers: [{ color: "#fdfcf8" }]
+    },
+    {
+      featureType: "road.highway",
+      elementType: "geometry",
+      stylers: [{ color: "#f8c967" }]
+    },
+    {
+      featureType: "road.highway",
+      elementType: "geometry.stroke",
+      stylers: [{ color: "#e9bc62" }]
+    },
+    {
+      featureType: "road.highway.controlled_access",
+      elementType: "geometry",
+      stylers: [{ color: "#e98d58" }]
+    },
+    {
+      featureType: "road.highway.controlled_access",
+      elementType: "geometry.stroke",
+      stylers: [{ color: "#db8555" }]
+    },
+    {
+      featureType: "road.local",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#806b63" }]
+    },
+    {
+      featureType: "transit.line",
+      elementType: "geometry",
+      stylers: [{ color: "#dfd2ae" }]
+    },
+    {
+      featureType: "transit.line",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#8f7d77" }]
+    },
+    {
+      featureType: "transit.line",
+      elementType: "labels.text.stroke",
+      stylers: [{ color: "#ebe3cd" }]
+    },
+    {
+      featureType: "transit.station",
+      elementType: "geometry",
+      stylers: [{ color: "#dfd2ae" }]
+    },
+    {
+      featureType: "water",
+      elementType: "geometry.fill",
+      stylers: [{ color: "#b9d3c2" }]
+    },
+    {
+      featureType: "water",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#92998d" }]
+    }
+  ]
+};
+
+export const AboutPageTemplate = ({ title, mapZoom, mapLng, mapLat, content, contentComponent }) => {
   const PageContent = contentComponent || Content
 
   return (
@@ -21,12 +135,29 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
           </div>
         </div>
       </div>
+
+      <div style={{ height: "100vh", width: "100%" }}>
+        <GoogleMap
+          center={{
+            lat: mapLat,
+            lng: mapLng
+          }}
+          zoom={mapZoom}
+          options={mapOptions}
+          bootstrapURLKeys={{
+            key: "AIzaSyCGidjP2EjD3tGLPBSp7KsHdcaoK3lI_Kc"
+          }}
+        ></GoogleMap>
+      </div>
     </section>
-  )
+  );
 }
 
 AboutPageTemplate.propTypes = {
   title: PropTypes.string.isRequired,
+  mapZoom: PropTypes.number.isRequired,
+  mapLat: PropTypes.number.isRequired,
+  mapLng: PropTypes.number.isRequired,
   content: PropTypes.string,
   contentComponent: PropTypes.func,
 }
@@ -39,6 +170,9 @@ const AboutPage = ({ data }) => {
       <AboutPageTemplate
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
+        mapZoom={post.frontmatter.mapZoom}
+        mapLat={post.frontmatter.mapLat}
+        mapLng={post.frontmatter.mapLng}
         content={post.html}
       />
     </Layout>
@@ -57,6 +191,9 @@ export const aboutPageQuery = graphql`
       html
       frontmatter {
         title
+        mapZoom
+        mapLat
+        mapLng
       }
     }
   }
