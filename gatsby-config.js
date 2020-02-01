93838c2b84dc82df9e1c9352a467926f9a6011e0
Mikolaj Dobrucki <mikolaj.dobrucki@gmail.com>
2020-02-01 16:42:33 +0100

    Update page title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Mossi Atelier',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Studio projektowania architektury wnętrz i wykonywania hiperrealistycznych wizualizacji.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
